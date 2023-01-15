5db2137497b7f4a9556d85b6e93d1a61f83dab03
Norah Lu <101174526+NalianLu@users.noreply.github.com>
2023-01-15 11:32:53 -0600

    Add ps0

---
+++ ps0_Simple_Python_Program/ps0_Norah.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 14 16:10:34 2023
+
+@author: 65159
+"""
+
+import math
+
+x = int(input('Enter number x: '))
+y = int(input('Enter number y: '))
+print('x**y =', x**y)
+print('log(x) =', math.log2(x))
